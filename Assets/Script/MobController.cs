2a50da14ca205142ded079916a1e977bfba546fb
jowansik <jowansik@naver.com>
2018-01-19 17:58:22 +0900

    1

--- Assets/Script/MobController.cs
+++ Assets/Script/MobController.cs
@@ -8,15 +8,18 @@ public class MobController : MonoBehaviour
     public float maxAggroRadius;
     public float attackRange;
 
+
     private Transform mobTR;
     private Transform playerTR;
     private bool life = true;
+    private Animator animator;
 
 	// Use this for initialization
 	void Start ()
     {
         mobTR = GetComponent<Transform>();
         playerTR = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        animator = GetComponent<Animator>();
         StartCoroutine(CheckMobState());
 	}
 	
@@ -32,7 +35,23 @@ public class MobController : MonoBehaviour
         {
             yield return new WaitForSeconds(0.2f);
 
+            float dist = Vector2.Distance(mobTR.position, playerTR.position);
+
             // 상태변경 코드
+            if (dist < aggroRadius && dist > attackRange)
+            {
+                Quaternion look = Quaternion.identity;
+                Vector3 dir = (playerTR.position - mobTR.position).normalized;
+                look.SetLookRotation(dir);
+
+                mobTR.rotation = look;
+
+                animator.SetBool("IsTracing", true);
+            }
+            else
+            {
+                animator.SetBool("IsTracing", false);
+            }
         }
     }
 }
