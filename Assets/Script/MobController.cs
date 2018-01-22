224c40fc0746190c5c309cc9626d878b87522dcf
jowansik <jowansik@naver.com>
2018-01-22 17:56:40 +0900

    1

--- Assets/Script/MobController.cs
+++ Assets/Script/MobController.cs
@@ -7,51 +7,80 @@ public class MobController : MonoBehaviour
     public float aggroRadius;
     public float maxAggroRadius;
     public float attackRange;
-
-
+    
     private Transform mobTR;
     private Transform playerTR;
     private bool life = true;
     private Animator animator;
 
-	// Use this for initialization
-	void Start ()
+    private IState currentState;
+
+    public Transform MobTR
+    {
+        get { return mobTR; }
+        set { mobTR = value; }
+    }
+
+    public Transform PlayerTR
+    {
+        get { return playerTR; }
+        set { playerTR = value; }
+    }
+
+    public Animator Animator
+    {
+        get { return animator; }
+        set { animator = value; }
+    }
+
+    private void Awake()
+    {
+        currentState = new IdleState();
+    }
+
+    // Use this for initialization
+    void Start()
     {
         mobTR = GetComponent<Transform>();
         playerTR = GameObject.FindWithTag("Player").GetComponent<Transform>();
         animator = GetComponent<Animator>();
         StartCoroutine(CheckMobState());
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+        currentState.Update();
+    }
 
     IEnumerator CheckMobState()
     {
-        while(life == true)
+        while (life == true)
         {
             yield return new WaitForSeconds(0.2f);
 
-            float dist = Vector2.Distance(mobTR.position, playerTR.position);
+            float dist = Vector3.Distance(mobTR.position, playerTR.position);
 
-            // 상태변경 코드
             if (dist < aggroRadius && dist > attackRange)
             {
-                Quaternion look = Quaternion.identity;
-                Vector3 dir = (playerTR.position - mobTR.position).normalized;
-                look.SetLookRotation(dir);
-
-                mobTR.rotation = look;
-
                 animator.SetBool("IsTracing", true);
+                ChangeState(new FollowState());
             }
             else
             {
                 animator.SetBool("IsTracing", false);
+                ChangeState(new IdleState());
             }
         }
+    }
+
+    public void ChangeState(IState newState)
+    {
+        if (currentState != null)
+            currentState.Exit();
+
+        currentState = newState;
+
+        currentState.Enter(this);
     }
 }
