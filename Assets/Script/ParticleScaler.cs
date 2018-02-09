b8e3b27497f2a1ac66874868bc4f9da1da674598
jowansik <jowansik@naver.com>
2018-02-09 18:59:57 +0900

    123

---
+++ Assets/Script/ParticleScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MirzaBeig.ParticleSystems;
+
+public class ParticleScaler : MonoBehaviour
+{
+    private ParticleSystem pS;
+    private ParticleSystems pSs;
+
+    public float scaleFactor;
+
+    // Use this for initialization
+    void Start ()
+    {
+        pS = GetComponent<ParticleSystem>();
+        pSs = GetComponent<ParticleSystems>();        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       // pS.main.scalingMode = ParticleSystemScalingMode.Hierarchy;
+	}
+}
